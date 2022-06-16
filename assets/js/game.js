a39b0603f24e67046e0ae127168ea3452ddb5392
mitchgm <mitchgm@bgsu.edu>
2022-06-15 20:16:52 -0700

    fixed shop function

--- assets/js/game.js
+++ assets/js/game.js
@@ -283,25 +283,26 @@ var enemyInfo = [
         // ask the player what they would like to do
 
         var shopOptionPrompt = window.prompt (
-            "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one: 'REFILL', 'UPGRADE' or 'LEAVE' to make a choice."
+          "Would you like to REFILL your health, UPGRADE your attack, or LEAVE the store? Please enter one 1 for REFILL, 2 for UPGRADE, or 3 for LEAVE."
         );
+        shopOptionPrompt = parseInt(shopOptionPrompt);
+
         // use switch to carry out action
         switch (shopOptionPrompt) {
-            case "refill" :
-                case "REFILL":
+            case 1 :
+               
                 playerInfo.refillHealth();
            
             break;
 
 
-            case "upgrade":
-                case "UPGRADE":
+            case 2:
                playerInfo.upgradeAttack();
                 break;
 
 
 
-                case "leave":
+                case 3:
                     window.alert("Leaving the store.");
 
                     // do nothing, end function
